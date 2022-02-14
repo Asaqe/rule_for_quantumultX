1100eed0576d06c20a9f9869de0be1f4f18472ee
Asaqe <87805027+Asaqe@users.noreply.github.com>
2022-02-14 19:08:55 +0800

    Add files via upload

---
+++ js/backup/1blocker 2.js
@@ -0,0 +1,16 @@
+{
+  "request_date": "2022-02-14T09:47:54Z",
+  "request_date_ms": 1644832074945,
+  "subscriber": {
+    "entitlements": {},
+    "first_seen": "2021-05-13T07:34:56Z",
+    "last_seen": "2022-02-14T09:46:49Z",
+    "management_url": null,
+    "non_subscriptions": {},
+    "original_app_user_id": "1C049599-1124-4677-A683-978C40445C7B",
+    "original_application_version": "1012",
+    "original_purchase_date": "2021-05-13T07:33:26Z",
+    "other_purchases": {},
+    "subscriptions": {}
+  }
+}
