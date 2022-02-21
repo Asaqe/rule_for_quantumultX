d7e50ed02d03f7bc93b4aa3a20df40ae247549b7
Asaqe <87805027+Asaqe@users.noreply.github.com>
2022-02-21 22:44:19 +0800

    Add files via upload

--- js/backup/filebox.js
+++ js/backup/filebox.js
@@ -1,2 +1,45 @@
-var sb={"request_date": "2020-06-05T11:54:41Z", "request_date_ms": 1591358081473, "subscriber": {"entitlements": {"filebox_pro": {"expires_date": "2030-02-18T07:52:54Z", "product_identifier": "com.premium.yearly", "purchase_date": "2020-02-11T07:52:54Z"}}, "first_seen": "2020-05-29T07:59:41Z", "last_seen": "2020-06-05T11:46:28Z", "management_url": null, "non_subscriptions": {}, "original_app_user_id": "RbhyxwVVYSgnnUEtme2444PjccJ3", "original_application_version": "5", "original_purchase_date": "2020-05-29T07:47:32Z", "other_purchases": {}, "subscriptions": {"com.premium.yearly": {"billing_issues_detected_at": null, "expires_date": "2030-02-18T07:52:54Z", "is_sandbox": false, "original_purchase_date": "2020-02-11T07:52:55Z", "period_type": "normal", "purchase_date": "2020-02-11T07:52:54Z", "store": "app_store", "unsubscribe_detected_at": null}}}};
-$done({body : JSON.stringify(ddgksf2013)});
+var objc = JSON.parse($response.body);
+
+    objc =
+
+{
+  "request_date_ms" : 1644807425219,
+  "request_date" : "2022-02-14T02:57:05Z",
+  "subscriber" : {
+    "non_subscriptions" : {
+      "filebox_pro" : [
+        {
+          "id" : "35cd5ede42",
+          "is_sandbox" : false,
+          "purchase_date" : "2022-02-13T10:28:11Z",
+          "original_purchase_date" : "2022-02-13T10:28:11Z",
+          "store" : "app_store"
+        }
+      ]
+    },
+    "first_seen" : "2022-02-13T10:30:14Z",
+    "original_application_version" : "100",
+    "other_purchases" : {
+      "filebox_pro" : {
+        "purchase_date" : "2022-02-13T10:28:11Z"
+      }
+    },
+    "management_url" : null,
+    "subscriptions" : {
+
+    },
+    "entitlements" : {
+      "filebox_pro" : {
+        "grace_period_expires_date" : null,
+        "purchase_date" : "2022-02-13T10:28:11Z",
+        "product_identifier" : "filebox_pro",
+        "expires_date" : null
+      }
+    },
+    "original_purchase_date" : "2022-02-02T22:54:45Z",
+    "original_app_user_id" : "$RCAnonymousID:0ab3738daba94750bcd43d1c99adcf39",
+    "last_seen" : "2022-02-13T10:30:14Z"
+  }
+};
+
+$done({body : JSON.stringify(objc)});
