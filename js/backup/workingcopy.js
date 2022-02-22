f3ef700efe28b07dd17519b09c9da761b99f1c26
Asaqe <87805027+Asaqe@users.noreply.github.com>
2022-02-22 12:28:42 +0800

    Update workingcopy.js

--- js/backup/workingcopy.js
+++ js/backup/workingcopy.js
@@ -1,23 +1,13 @@
 /*
 利用 GitHub Education 解锁 Working Copy
 
-启用此脚本后再 Safari 打开 https://workingcopy.app/education/ 点击「tap here to access all pro features.」中的 here 跳转到 Working Copy，登录自己的 GitHub 账号即可解锁
-
-http-response ^https:\/\/education\.github\.com\/api\/user$ requires-body=1,max-size=0,script-path=/*
-利用 GitHub Education 解锁 Working Copy
 
 启用此脚本后再 Safari 打开 https://workingcopy.app/education/ 点击「tap here to access all pro features.」中的 here 跳转到 Working Copy，登录自己的 GitHub 账号即可解锁
 
-http-response ^https:\/\/education\.github\.com\/api\/user$ requires-body=1,max-size=0,script-path=scripts/WorkingCopy.js
+http-response ^https:\/\/education\.github\.com\/api\/user$ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Asaqe/rule_for_quantumultX/master/js/backup/workingcopy.js
 
 MitM = education.github.com
 
-作者 @ImSingee，请勿外传
-
-
-$done({body: '{"student": true}'})
-
-MitM = education.github.com
 
 作者 @ImSingee，请勿外传
 */
