97c9293a99d9b6ada0569e310203bea3cf8b6a59
Asaqe <87805027+Asaqe@users.noreply.github.com>
2022-03-16 08:14:12 +0800

    Add files via upload

---
+++ js/backup/gitmind.js
@@ -0,0 +1,33 @@
+var obj= {
+  "status": 200,
+  "message": "success",
+  "data": {
+    "user": {
+      "user_id": 44134317,
+      "phone": "",
+      "avatar": "http:\/\/thirdqq.qlogo.cn\/g?b=oidb&k=HYwIgCST4ibXMvv47Kqhojg&s=40",
+      "nickname": "Asaqe",
+      "language": "hk",
+      "last_login_time": 1647346609,
+      "is_vip": 1,
+      "passport_license_type": "yearly",
+      "vip_expired_at": "2222-02-22 20:16",
+      "metas": {
+        "file_num": 0,
+        "cooperation_num": 0,
+        "accept_cooperation": "1",
+        "hide_expander": "0",
+        "hover_showText": "0",
+        "order_type": "updated_at",
+        "sort_by": "desc",
+        "view_type": "0",
+        "theme": "",
+        "default_set": "",
+        "use_random_theme": 1,
+        "show_comment": 1,
+        "word_wrap": 0
+      }
+    }
+  }
+}
+$done({body: JSON.stringify(obj)});
