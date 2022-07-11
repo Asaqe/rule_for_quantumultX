7d2f3244278701a992a288c8c533b6b13e703d53
Asaqe <87805027+Asaqe@users.noreply.github.com>
2022-07-11 17:14:07 +0800

    Update revenuecat.js

--- js/backup/revenuecat.js
+++ js/backup/revenuecat.js
@@ -9,18 +9,37 @@ if(url.endsWith("offerings")||url.endsWith("products")) {
 } else {
 	// Noto笔记Lifetime
 	if (url.indexOf("8DA3F946-C754-411E-8928-C9D1DAEC1CD1")!=-1) {
-		obj["subscriber"]["entitlements"]["pro"]={
-			"expires_date": null,
-			"product_identifier": "com.lkzhao.editor.full.deal",
-			"purchase_date": "2019-12-01T00:00:00Z"
-		};
-		obj["subscriber"]["non_subscriptions"]["com.lkzhao.editor.full.deal"]=[{
-			"id": "12345qwert",
-			"is_sandbox": false,
-			"original_purchase_date": "2019-12-01T00:00:00Z",
-			"purchase_date": "2019-12-01T00:00:00Z",
-			"store": "app_store"
-		}];
+		obj = {"request_date": "2020-06-05T11:54:41Z",
+  "request_date_ms": 1591358081473,
+  "subscriber": {
+    "entitlements": {
+      "pro": {
+        "expires_date": "2030-02-18T07:52:54Z",
+        "product_identifier": "com.gocas.byroon.yearly",
+        "purchase_date": "2020-02-11T07:52:54Z"
+      }
+    },
+    "first_seen": "2020-05-29T07:59:41Z",
+    "last_seen": "2020-06-05T11:46:28Z",
+    "management_url": null,
+    "non_subscriptions": {},
+    "original_app_user_id": "RbhyxwVVYSgnnUEtme2444PjccJ3",
+    "original_application_version": "5",
+    "original_purchase_date": "2020-05-29T07:47:32Z",
+    "other_purchases": {},
+    "subscriptions": {
+      "com.gocas.byroon.yearly": {
+        "billing_issues_detected_at": null,
+        "expires_date": "2030-02-18T07:52:54Z",
+        "is_sandbox": false,
+        "original_purchase_date": "2020-02-11T07:52:55Z",
+        "period_type": "normal",
+        "purchase_date": "2020-02-11T07:52:54Z",
+        "store": "app_store",
+        "unsubscribe_detected_at": null
+      }
+    }
+  }
 	}
 	// Airmail Premium
 	if (url.indexOf("5647911E-B243-48C9-A25C-29116412A20D")!=-1) {
