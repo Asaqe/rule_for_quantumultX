cce81018f462c086ff6457dfc8a2015e775f7bc6
Asaqe <87805027+Asaqe@users.noreply.github.com>
2022-08-29 08:49:38 +0800

    Update cubox.js

--- js/backup/cubox.js
+++ js/backup/cubox.js
@@ -1,14 +1,14 @@
-/*
+/**********
 [rewrite_local]
 
 ^https:\/\/cubox.pro\/c\/api\/userInfo url script-response-body https://raw.githubusercontent.com/Asaqe/rule_for_quantumultX/master/js/backup/cubox.js
 
-mitm = cubox.pro
-/*
+mitm=cubox.pro
+***********/
 
 var body = $response.body;
 var obj = JSON.parse(body);
 obj.data.payTime = "2021-01-01T08:00:00+08:00";
 obj.data.isExpire = false;
-obj.data.expireTime = "2099-01-01T08:00:00+08:00";
+obj.data.expireTime = "2039-01-01T08:00:00+08:00";
 $done({body: JSON.stringify(obj)});
