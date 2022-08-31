a93176c5bb864f377b46980cdbd702f48a3f8231
Asaqe <87805027+Asaqe@users.noreply.github.com>
2022-08-31 23:34:40 +0800

    Update cubox.js

--- js/backup/cubox.js
+++ js/backup/cubox.js
@@ -11,5 +11,5 @@ var body = $response.body;
 var obj = JSON.parse(body);
 obj.data.payTime = "2021-01-01T08:00:00+08:00";
 obj.data.isExpire = false;
-obj.data.expireTime = "2039-01-01T08:00:00+08:00";
+obj.data.expireTime = "2099-01-01T08:00:00+08:00";
 $done({body: JSON.stringify(obj)});
