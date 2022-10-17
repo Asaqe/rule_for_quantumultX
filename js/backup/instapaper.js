84b24cd509f9ba02464f18c36d66f92d8b9cb423
Asaqe <87805027+Asaqe@users.noreply.github.com>
2022-10-17 22:05:38 +0800

    Add files via upload

---
+++ js/backup/instapaper.js
@@ -0,0 +1,7 @@
+/*
+[rewrite_local]
+https://www.instapaper.com/api/subscription_status url script-response-body https://raw.githubusercontent.com/zqzess/rule_for_quantumultX/master/js/backup/instapaper.js
+[mimt]
+hostname=www.instapaper.com
+*/
+body = $response.body.replace(/.+/,"ipsubnow=1585578158&ipsubexp=4079167311&ipsub=1");$done({body});
